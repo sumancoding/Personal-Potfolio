4ee7602dcdca205cc6dabd91cda8b5a00a5b6d69
sumancoding <progress.suman@gmail.com>
2022-04-24 02:13:16 +0300

    wireframe started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Stack } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Stack direction="row" spacing={2} justifyContent="space-around">
+        <h1>Left</h1>
+        <h1>Right</h1>
+      </Stack>
+    </Box>
   );
 }
 
