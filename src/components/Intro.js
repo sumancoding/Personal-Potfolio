96844457e24d88fa8b6d6f87e7b3c87b7fea0a99
sumancoding <progress.suman@gmail.com>
2022-04-25 23:50:31 +0300

    working with animations

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -16,6 +16,17 @@ const Intro = () => {
               <div className="i-title-item">UI/UX design</div>
             </div>
           </div>
+          <div className="i-description">
+            👋 Hi there, I'm Suman Malla (@sumancoding) and I'm learning
+            Fullstack development. My fondness with web space was pivotal for
+            incepting this new career and I'm happy thus far. I've been using
+            MERN stack for quite some time now and i'm still learning it. My
+            inclination is more towards frontend side. I also enjoy wireframes,
+            prototyping along with UI/UX design. Now adays i'm focusing more on
+            the backend side. I'm looking for any kind of opportunities.I want
+            to get started working by learning and sharing knowledge and
+            experience.
+          </div>
         </div>
       </div>
       <div className="i-right">right</div>
