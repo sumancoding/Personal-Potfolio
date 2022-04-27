12b873fafa911e64798ce467050461603c958692
sumancoding <progress.suman@gmail.com>
2022-04-28 00:32:03 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import Intro from "./components/Intro";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import "./index.css";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Intro />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 };
 
